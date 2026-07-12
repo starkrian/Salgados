b0dc7359f74191b140d0aea703b81382fde1c31c
starkrian <rbwp0o@gmail.com>
2026-07-12 15:41:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,31 @@
-const CACHE_NAME = 'salgados-cache-v1';
+const CACHE_NAME = 'salgados-cache-v3'; // Mudamos para v3 para forçar a atualização
 const assets = [
-  './',
   './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap'
+  './manifest.json'
 ];
 
-// Instala e guarda os ficheiros na memória do telemóvel
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    }).then(() => self.skipWaiting()) 
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Apaga a memória v2 antiga
+          }
+        })
+      );
     })
   );
 });
 
-// Serve os ficheiros a partir da memória se estiver offline
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
